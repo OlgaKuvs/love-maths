9d7b3ea5f10bdd8d76dc8586f6d9237c56bcb34c
Olga Kuvsincikova <olja_kuv@inbox.lv>
2023-03-31 22:09:15 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,13 @@ document.addEventListener("DOMContentLoaded", function(){
         });
     }
 
-    runGame("multiply");
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if(event.key === "Enter") {
+            checkAnswer(); 
+        }
+    })
+
+    runGame("addition");
 });
 
 /**
@@ -24,6 +30,10 @@ document.addEventListener("DOMContentLoaded", function(){
  */
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Creates 2 random numbers between 1 and 25
 
     let num1 = Math.floor(Math.random() *25) + 1;
