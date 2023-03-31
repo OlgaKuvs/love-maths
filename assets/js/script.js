edabc979a41815c16389b4f841b7047f9811143b
Olga Kuvsincikova <olja_kuv@inbox.lv>
2023-03-31 21:46:15 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,8 +33,8 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if(gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else if(gameType === "substract") {
-        displaySubstractQuestion(num1, num2);
+    } else if(gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else if(gameType === "division") {
         displayDivisionQuestion(num1, num2);
     }    
@@ -80,6 +80,8 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, "addition"];
     } else if(operator === "x") {
         return [operand1 * operand2, "multiply"];
+    } else if(operator === "-") {
+        return [operand1 - operand2, "subtract"];
     }
     
     else {
@@ -110,8 +112,11 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "+";
 }
 
-function displaySubstractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
+    
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
