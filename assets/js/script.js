29be73864121726db4ed29690770d3f2fbc487ec
Olga Kuvsincikova <olja_kuv@inbox.lv>
2023-03-31 17:33:40 +0000

    Add event listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Wait for the DOM to finish loading before running the game
+// Get the buttons elements and event listeners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+
+
 function runGame() {
 
 }
@@ -8,6 +28,7 @@ function checkAnswer() {
 
 function calculateCorrectAnswer() {
     
+
 }
 
 function incrementScore() {
@@ -27,5 +48,9 @@ function displaySubstractQuestion() {
 }
 
 function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
 
 }
